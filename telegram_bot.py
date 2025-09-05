27cd829c18b1ae88a9e42e06dd81c4b1c8d9c6e4
Niebieski <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-09-05 17:33:53 +0300

    Refactor send_photo function to improve error handling and maintainability

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,22 +9,21 @@ from telegram import Bot
 from download_comics import save_rand_comic
 
 
-def send_photo(bot, chat_id, image_path: Path, caption: str):
-    try:
-        with image_path.open("rb") as f:
-            bot.send_photo(chat_id=chat_id, photo=f, caption=caption)
-    except FileNotFoundError:
-        pass
+def send_photo(bot, chat_id, image_path: Path, caption):
+    with image_path.open("rb") as f:
+        bot.send_photo(chat_id=chat_id, photo=f, caption=caption)
 
 
 def main():
     load_dotenv()
     bot = Bot(token=os.environ["TG_BOT_TOKEN"])
-    caption, image_path = save_rand_comic()
-    send_photo(bot, os.environ["TG_CHAT_ID"], image_path, caption)
-
-    with suppress(FileNotFoundError):
-            image_path.unlink()
+    
+    try:
+        caption, image_path = save_rand_comic()
+        send_photo(bot, os.environ["TG_CHAT_ID"], image_path, caption)
+    finally:
+        with suppress(FileNotFoundError):
+                image_path.unlink()
 
 
 if __name__ == "__main__":
